ff94638088f7cd105b0cb63c64109fa298041624
Akshansh Garg <akshanshgarg18@gmail.com>
2024-11-26 20:25:45 +0530

    folder structure added

--- template.py
+++ template.py
@@ -27,4 +27,19 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
+    "README.md"
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
